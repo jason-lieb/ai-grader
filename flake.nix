49a369584f9759e329d6b155003172bb7b9f15bf
Jason Lieb <Jason.lieb@outlook.com>
2025-12-12 22:50:35 -0500

    Init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+            corepack
+            nodejs_22
+            # Required by `node-gyp`
+            python3
+          ];
+        };
+      });
+    };
+}
