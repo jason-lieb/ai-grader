8e28ec26e3d34e4a7a1852414184bafc7e62a7c3
Jason Lieb <Jason.lieb@outlook.com>
2025-12-12 23:16:09 -0500

    update config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: ["src/bin.ts"],
   clean: true,
-  publicDir: true,
   treeshake: "smallest",
   external: ["@parcel/watcher"]
 })
